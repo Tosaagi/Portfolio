2a9272e01fb54c822cdf1a11dc8cc94cc7c71e56
Ariva Alvitto <arivaalvitto15@gmail.com>
2025-06-05 10:41:42 +0700

    Change bottom screen gradient overlay color to black

--- components/ScrollIndicator.jsx
+++ components/ScrollIndicator.jsx
@@ -21,7 +21,7 @@ function ScrollIndicator() {
     <div
       className={`flex fixed -bottom-20 left-0 w-full h-[180px] justify-center items-center transition-opacity duration-500 ${
         show ? "opacity-100" : "opacity-0"
-      } bg-gradient-to-t from-[#121212] to-transparent z-50`}
+      } bg-gradient-to-t from-black to-transparent z-50`}
     >
       <PiMouseScroll className="text-3xl text-mainaccent animate-bounce mt-15" />
     </div>
